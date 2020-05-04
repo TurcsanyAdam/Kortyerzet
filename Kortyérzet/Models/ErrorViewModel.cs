3ce09986bc631cdf15c9b6f35605899d0f71b6e4
Ádám Turcsány <turcsanyadam@gmail.com>
2020-05-04 15:58:27 +0200

    Add project files.

---
+++ Kortyérzet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kortyérzet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
